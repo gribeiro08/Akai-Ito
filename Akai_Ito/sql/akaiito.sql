f33aa72c3f92f441b499b3b363124f6206cdbfa9
Gabriela Ribeiro <gr08.ribeiro@gmail.com>
2023-05-12 20:15:41 -0300

    Update akaiito.sql

--- Akai_Ito/sql/akaiito.sql
+++ Akai_Ito/sql/akaiito.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 12-Maio-2023 às 04:02
+-- Tempo de geração: 13-Maio-2023 às 01:12
 -- Versão do servidor: 10.4.25-MariaDB
 -- versão do PHP: 8.1.10
 
@@ -30,19 +30,12 @@ SET time_zone = "+00:00";
 CREATE TABLE `anunciante` (
   `id` int(11) NOT NULL,
   `username` varchar(50) NOT NULL,
-  `usertype` varchar(50) NOT NULL,
   `data_nasc` date NOT NULL,
-  `password` varchar(50) NOT NULL,
   `full_name` varchar(50) DEFAULT NULL,
-  `anuncio` varchar(50) DEFAULT NULL
+  `password` varchar(50) NOT NULL,
+  `anuncio` varchar(50) NOT NULL,
+  `usertype` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Extraindo dados da tabela `anunciante`
---
-
-INSERT INTO `anunciante` (`id`, `username`, `usertype`, `data_nasc`, `password`, `full_name`, `anuncio`) VALUES
-(1, 'Clodovaldo', 'anunciante', '1996-03-30', 'Clodo123', 'Clodovaldo da Silva', '');
 
 -- --------------------------------------------------------
 
@@ -53,23 +46,38 @@ INSERT INTO `anunciante` (`id`, `username`, `usertype`, `data_nasc`, `password`,
 CREATE TABLE `jogador` (
   `id` int(11) NOT NULL,
   `username` varchar(50) NOT NULL,
-  `usertype` varchar(50) NOT NULL,
   `data_nasc` date NOT NULL,
   `full_name` varchar(50) DEFAULT NULL,
   `password` varchar(50) NOT NULL,
-  `story_progress` int(50) DEFAULT NULL
+  `story_progress` int(50) DEFAULT NULL,
+  `usertype` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `jogador`
+--
+
+INSERT INTO `jogador` (`id`, `username`, `data_nasc`, `full_name`, `password`, `story_progress`, `usertype`) VALUES
+(2, 'jogadorzinho', '2012-12-07', 'Juca Junior', 'Juca1234', NULL, 'player');
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `usertype`
+-- Estrutura da tabela `tipo_de_usuario`
 --
 
-CREATE TABLE `usertype` (
-  `id` int(11) NOT NULL,
-  `usertype` varchar(50) NOT NULL
+CREATE TABLE `tipo_de_usuario` (
+  `id` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `tipo_de_usuario`
+--
+
+INSERT INTO `tipo_de_usuario` (`id`) VALUES
+('admin'),
+('anunciante'),
+('player');
 
 -- --------------------------------------------------------
 
@@ -80,16 +88,9 @@ CREATE TABLE `usertype` (
 CREATE TABLE `user_adm` (
   `id` int(11) NOT NULL,
   `username` varchar(50) NOT NULL,
-  `usertype` varchar(50) NOT NULL,
-  `password` varchar(50) NOT NULL
+  `password` varchar(50) NOT NULL,
+  `usertype` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Extraindo dados da tabela `user_adm`
---
-
-INSERT INTO `user_adm` (`id`, `username`, `usertype`, `password`) VALUES
-(1, 'admin', 'admin', '1234');
 
 --
 -- Índices para tabelas despejadas
@@ -99,25 +100,28 @@ INSERT INTO `user_adm` (`id`, `username`, `usertype`, `password`) VALUES
 -- Índices para tabela `anunciante`
 --
 ALTER TABLE `anunciante`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `usertype` (`usertype`);
 
 --
 -- Índices para tabela `jogador`
 --
 ALTER TABLE `jogador`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `usertype` (`usertype`);
 
 --
--- Índices para tabela `usertype`
+-- Índices para tabela `tipo_de_usuario`
 --
-ALTER TABLE `usertype`
+ALTER TABLE `tipo_de_usuario`
   ADD PRIMARY KEY (`id`);
 
 --
 -- Índices para tabela `user_adm`
 --
 ALTER TABLE `user_adm`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `usertype` (`usertype`);
 
 --
 -- AUTO_INCREMENT de tabelas despejadas
@@ -127,25 +131,41 @@ ALTER TABLE `user_adm`
 -- AUTO_INCREMENT de tabela `anunciante`
 --
 ALTER TABLE `anunciante`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de tabela `jogador`
 --
 ALTER TABLE `jogador`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT de tabela `usertype`
---
-ALTER TABLE `usertype`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de tabela `user_adm`
 --
 ALTER TABLE `user_adm`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restrições para despejos de tabelas
+--
+
+--
+-- Limitadores para a tabela `anunciante`
+--
+ALTER TABLE `anunciante`
+  ADD CONSTRAINT `anunciante_ibfk_1` FOREIGN KEY (`usertype`) REFERENCES `tipo_de_usuario` (`id`);
+
+--
+-- Limitadores para a tabela `jogador`
+--
+ALTER TABLE `jogador`
+  ADD CONSTRAINT `jogador_ibfk_1` FOREIGN KEY (`usertype`) REFERENCES `tipo_de_usuario` (`id`);
+
+--
+-- Limitadores para a tabela `user_adm`
+--
+ALTER TABLE `user_adm`
+  ADD CONSTRAINT `user_adm_ibfk_1` FOREIGN KEY (`usertype`) REFERENCES `tipo_de_usuario` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
